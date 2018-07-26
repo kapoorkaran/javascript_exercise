f7881585b07e20990c14883f2f10a4b0df7d2e37
kapoorkaran <kapoorkaran612@gmail.com>
2018-07-26 17:28:47 +0530

    string exercise

---
+++ strings/sol3.js
@@ -0,0 +1,7 @@
+var str="karan kapoor bhavya kanak";
+var tarr = new Array();
+
+tarr= str.split(" ");
+console.log(tarr);
+
+
---
+++ strings/sol6.js
@@ -0,0 +1,7 @@
+var email="kapoorkaran@gmail.com";
+splitemail= email.split("@");
+part1=splitemail[0];
+half=part1.length/2;
+part1=part1.substring(0,(part1.length-half))
+console.log(part1+"....@"+splitemail[1]);
+
